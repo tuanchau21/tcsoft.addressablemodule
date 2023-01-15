0470955de6a7456c622ce803caf16d04ac2d2ebe
Tuan Chau <sheepw76@gmail.com>
2023-01-15 01:12:10 +0100

    'init'

---
+++ Runtime/AddressableModuleInstaller.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using Zenject;
+
+public class AddressableModuleInstaller : Installer<AddressableModuleInstaller>
+{
+    public override void InstallBindings()
+    {
+        
+    }
+}
